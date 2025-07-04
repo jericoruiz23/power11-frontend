7f2de92fdd6d1def92bd995a4f64e36ca370a1ae
Benjamin Ruiz <Preventa10.ec@nexsysla.com>
2025-07-04 12:22:32 -0500

    Theme v2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -64,23 +64,34 @@ export default function Form() {
 
     return (
         <>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 3, mt: 9 }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    mb: 3,
+                    mt: 9,
+                    flexWrap: 'wrap',
+                    gap: 2,
+                }}
+            >
                 <img
                     src="https://www.nexsysla.com/ec/wp-content/uploads/sites/8/2022/06/nexsys-logo-light-2023.png"
-                    alt="Logo"
-                    style={{ height: 40, marginRight: 16 }}
+                    alt="Logo Nexsys"
+                    style={{ maxWidth: '100px', height: 'auto' }}
                 />
                 <img
                     src="https://cdn-icons-png.flaticon.com/512/43/43694.png"
-                    alt="Logo"
-                    style={{ height: 40, marginRight: 16 }}
+                    alt="Icono"
+                    style={{ maxWidth: '100px', height: 'auto' }}
                 />
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/2560px-IBM_logo.svg.png"
-                    alt="Logo"
-                    style={{ height: 40, marginRight: 16 }}
+                    alt="Logo IBM"
+                    style={{ maxWidth: '100px', height: 'auto' }}
                 />
             </Box>
+
             <Box p={3} maxWidth={500} mx="auto">
                 <Typography variant="h4" gutterBottom align="center" fontWeight="bold" sx={{ fontFamily: 'Arial, sans-serif' }}>
                     REGISTRO
